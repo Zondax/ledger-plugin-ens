c0ff7825a6aeab0af8d9f525508de3cc5a6b6e54
Carlos Medeiros <carlos.medeiros@zondax.ch>
2023-10-23 08:22:11 +0100

    update tests

---
+++ tests/src/set_name_long_34bytes.test.ts
@@ -0,0 +1,38 @@
+import { processTest, populateTransaction } from "./test.fixture";
+
+const contractName = "ReverseRegistrar"; // <= Name of the smart contract
+
+const testLabel = "reverse_registrar_set_name_34bytes"; // <= Name of the test
+const testDirSuffix = "reverse_registrar_set_name_34bytes"; // <= directory to compare device snapshots to
+const testNetwork = "ethereum";
+const signedPlugin = false;
+
+const contractAddr = "0x084b1c3c81545d370f3634392de611caabff8148"; // <= Address of the smart contract
+const chainID = 1;
+
+// From : https://etherscan.io/tx/0x905e106763556d0cb16d1fc11ab13d75cf5b1227e0480098b909bba50c4271b8
+const inputData = "0xc47f00270000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000002241414141414141414141414141414141414141414141414141414141414141414242000000000000000000000000000000000000000000000000000000000000";
+// Create serializedTx and remove the "0x" prefix
+const value = "3.1";
+const serializedTx = populateTransaction(contractAddr, inputData, chainID, value);
+const devices = [
+   {
+     name: "nanos",
+     label: "Nano S",
+     steps: 6, // <= Define the number of steps for this test case and this device
+   },
+  {
+    name: "nanox",
+    label: "Nano X",
+    steps: 4, // <= Define the number of steps for this test case and this device
+  },
+  {
+     name: "nanosp",
+     label: "Nano S+",
+     steps: 4 // <= Define the number of steps for this test case and this device
+   },
+];
+
+devices.forEach((device) =>
+  processTest(device, contractName, testLabel, testDirSuffix, "", signedPlugin, serializedTx, testNetwork)
+);
