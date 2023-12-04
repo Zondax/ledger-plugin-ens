f5322bd3b34a63fe598f3027cbc26372a23b07ae
Carlos Medeiros <carlos.medeiros@zondax.ch>
2023-12-04 15:54:46 +0000

    fix cmakelists

--- src/ens_plugin.h
+++ src/ens_plugin.h
@@ -220,6 +220,4 @@ typedef struct context_t {
     ens_tx_t tx;
 } context_t;
 
-// Piece of code that will check that the above structure is not bigger than 5 * 32. Do not remove
-// this check.
-_Static_assert(sizeof(context_t) <= 5 * 32, "Structure of parameters too big.");
+ASSERT_SIZEOF_PLUGIN_CONTEXT(context_t);
